51a5af11a7172f7852b652242d2a854ee3ff5a0e
Matej Ukmar <matejukmar@gmail.com>
2024-06-05 14:13:50 +0100

    Updated package version and name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='scaler_pics',
-    version='0.1.0',
+    name='scaler-pics',
+    version='0.1.1',
     description='A Python library for image scaling and conversion',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
